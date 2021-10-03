b53acf8cff925f2a9282ce4bb84982ea0929c8d9
seung <38664481+seunggil1@users.noreply.github.com>
2021-10-04 03:32:36 +0900

    Update callApi.py

--- callApi.py
+++ callApi.py
@@ -4,14 +4,22 @@ from datetime import datetime, tzinfo
 import pytz
 from time import sleep
 
-# azure 서버에서 호출
+# use in azure vm machine
 # azure pw : rkdtmdrlf1plato!
+
+# chmod 755 callApi.py
+# nohup python3 -u callApi.py &
+
+# 실행중인 프로세스 보고 종료
+# ps -ef | grep py
+# sudo kill 000
 kst = pytz.timezone('Asia/Seoul')
 pre = datetime(1990,1,1, tzinfo = kst)
 now = datetime.now(kst)
 
 lastNotifyDay = 0
 while True:
+    print(now.__str__())
     if now.hour >= 8:
         if (now - pre).days * 60 * 60 * 24 + (now - pre).seconds >= 60 * 240 :
             response = requests.post('https://us-central1-plato-calendar.cloudfunctions.net/requestSync',
