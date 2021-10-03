5ae0a9f0149a609568a05503cdd1bcc242d39fff
seung <38664481+seunggil1@users.noreply.github.com>
2021-10-03 23:42:51 +0900

    azure pwd

--- requests.py
+++ requests.py
@@ -1,5 +1,6 @@
 import requests
 import json
+# azure pw : rkdtmdrlf1plato!
 response = requests.post('https://us-central1-plato-calendar.cloudfunctions.net/requestSync',
     data= {"password":"password"}
 )
